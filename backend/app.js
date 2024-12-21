4b57dc546f05fe4acdac506d2a9694d3f3cc3040
darkttyu <ctrlfrz0710@gmail.com>
2024-12-22 00:26:32 +0800

    Testing Complete

--- backend/app.js
+++ backend/app.js
@@ -15,7 +15,8 @@ app.use(cookieParser());
 app.use(cors({
   origin: 'http://localhost:3000',
   methods: ['GET', 'POST', 'PUT', 'DELETE'],
-  allowedHeaders: ['Content-Type', 'Authorization']
+  allowedHeaders: ['Content-Type', 'Authorization'],
+  credentials: true
 }));
 
 app.use("/api/auth", authRoutes);
