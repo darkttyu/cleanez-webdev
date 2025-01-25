631d06f6a96a0e032ca5312ead286ba6c909f5dc
darkttyu <ctrlfrz0710@gmail.com>
2025-01-26 03:24:18 +0800

    inaantok na ko

--- frontend/src/components/AppointmentInfo.js
+++ frontend/src/components/AppointmentInfo.js
@@ -39,7 +39,7 @@ const AppointmentInfo = ({appID, setShowAppInfo}) => {
     useEffect(() => {
         const fetchAppointment = async (id) => {
             try {
-                const response = await axios.get(`http://localhost:5000/api/user/viewAppointment/${id}`);
+                const response = await axios.get(`https://cleanez-api.vercel.app/api/user/viewAppointment/${id}`);
     
                 setData(response.data.data);
             } catch (e) {
