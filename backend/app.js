bb9d4c22ee50f701ddf995ea2d6fa99ba7a6893d
darkttyu <ctrlfrz0710@gmail.com>
2025-01-26 11:10:56 +0800

    backend config

--- backend/app.js
+++ backend/app.js
@@ -26,10 +26,12 @@ app.use(express.urlencoded({ extended: true }));
 // CORS middleware configuration for handling cross-origin requests
 app.use(cors({
   origin: "https://cleanez.vercel.app", // Allow requests from the frontend
-  methods: ['GET', 'POST', 'PUT', 'DELETE'], // Allowed HTTP methods
+  methods: ['GET', 'POST', 'PUT', 'OPTIONS', 'DELETE'], // Allowed HTTP methods
   allowedHeaders: ['Content-Type', 'Authorization'], // Allowed headers in requests
   credentials: true // Allow credentials (cookies, HTTP authentication) in requests
 }));
+
+app.options('*', cors());
 
 // Route handlers for different API endpoints
 app.use("/api/auth", authRoutes); // Handles authentication-related routes
