2641d7974a190aa77466f8f43c15d1468de7ca09
reinchisdog <reindf164@gmail.com>
2025-01-28 18:42:06 +0800

    Updating Schedule

--- frontend/src/pages/UserPages/WorkerSchedule.js
+++ frontend/src/pages/UserPages/WorkerSchedule.js
@@ -1,4 +1,197 @@
 const WorkerSchedule = () => {
+    const [workerDetails, setWorkerDetails] = useState(null);
+    const [serviceList, setServiceList] = useState([]);
+    const [areaList, setAreaList] = useState([]);
+    const [timeList, setTimeList] = useState([]);
+
+    const [dayValues, setDayValues] = useState([]);
+    const [timeValues, setTimeValues] = useState([]);
+
+    const [newWorkerSchedule, setNewWorkerSchedule] = useState({
+        serviceCategory: "0",
+        workerAvailability: {
+            areaAssigned: "0",
+            day: [],
+            startTime: []
+        }
+    });
+
+    const [editMode, setEditMode] = useState(false);
+
+    // Functions --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- 
+    // --- Fetches Worker Detail from Database;
+    const fetchWorkerDetail = async () => {
+        try {
+            const token = localStorage.getItem('token');
+            const response = await axios.get(`http://localhost:5000/api/worker/getWorkerDetails`, {
+                headers: { 'Authorization': `Bearer ${token}` }
+            })
+
+            // console.log(response.data.worker);
+            setWorkerDetails(response.data.worker);
+        } catch (e) {
+            console.log(e);
+            setWorkerDetails(null);
+        }
+    };
+
+    const fetchServices = async () => {
+        try {
+            const response = await axios.get(`http://localhost:5000/api/appointment/getServices`);
+
+            // console.log(response.data.services);
+            setServiceList(response.data.services);
+        } catch (e) {
+            console.log(e);
+            setServiceList([]);
+        }
+    };
+
+    const fetchAreas = () => {
+        const selectedService = serviceList.find(
+            (service) => service.serviceName === newWorkerSchedule.serviceCategory
+        );
+    
+        if (selectedService && selectedService.areaDetails !== areaList) {
+            // console.log("Selected Area: ", selectedService.areaDetails);
+            setAreaList(selectedService.areaDetails);
+        }
+    };
+    
+    const fetchTime = () => {
+        const selectedArea = areaList.find(
+            (area) => area.sizeOfArea === newWorkerSchedule.workerAvailability.areaAssigned
+        );
+
+        if (selectedArea && selectedArea.startTime !== timeList) {
+            // console.log("Selected Time: ", selectedArea.startTime);
+            setTimeList(selectedArea.startTime);
+        }
+    }
+
+    const convertTime = (time) => {
+        const hour = parseInt(time.slice(0, 2)); // Extract and convert the hour part to a number
+        const mins = time.slice(2); // Extract the minutes part as is
+    
+        if (hour === 0) {
+            return `12:${mins} AM`; // Midnight
+        } else if (hour < 12) {
+            return `${hour.toString().padStart(2, '0')}:${mins} AM`; // Morning
+        } else if (hour === 12) {
+            return `12:${mins} PM`; // Noon
+        } else {
+            const convertedHour = (hour - 12).toString().padStart(2, '0'); // Convert to 12-hour format and pad with 0
+            return `${convertedHour}:${mins} PM`; // Afternoon and evening
+        }
+    };
+
+    const handleDayChanges = (e) => {
+        const {value, checked} = e.target;
+
+        if (checked) {
+            setDayValues((prev) => [...prev, value]);
+        } else {
+            setDayValues((prev) => prev.filter((val) => val !== value));
+        }
+    }
+    const handleTimeChanges = (e) => {
+        const {value, checked} = e.target;
+
+        if (checked) {
+            setTimeValues((prev) => [...prev, value]);
+        } else {
+            setTimeValues((prev) => prev.filter((val) => val !== value));
+        }
+    }
+    useEffect(() => {
+        // console.log("Day Values", dayValues);
+        setNewWorkerSchedule({
+            ...newWorkerSchedule,
+            workerAvailability: {
+                ...newWorkerSchedule.workerAvailability,
+                day: dayValues
+            },
+        }) 
+    }, [dayValues])
+    useEffect(() => {
+        // console.log("Time Values", timeValues);
+        setNewWorkerSchedule({
+            ...newWorkerSchedule,
+            workerAvailability: {
+                ...newWorkerSchedule.workerAvailability,
+                startTime: timeValues
+            },
+        }) 
+    }, [timeValues])
+
+    useEffect(() => {
+        if (areaList.length > 0 && newWorkerSchedule.workerAvailability.areaAssigned !== "0") {
+            fetchTime();
+        }
+    }, [newWorkerSchedule, areaList])
+
+    useEffect(() => {
+        if (serviceList.length > 0 && newWorkerSchedule.serviceCategory !== "0") {
+            fetchAreas();
+        }
+    }, [newWorkerSchedule, serviceList]);
+    
+    useEffect(() => {
+        console.log("OLD: ", workerDetails)
+        console.log("NEW: ", newWorkerSchedule);
+    }, [newWorkerSchedule])
+    
+    useEffect(() => {
+        // console.log("Initial Worker Deets: ", workerDetails);
+        if (workerDetails) {
+            setNewWorkerSchedule({
+                serviceCategory: workerDetails.serviceCategory,
+                workerAvailability: {
+                    areaAssigned: workerDetails.workerAvailability.areaAssigned,
+                    day: workerDetails.workerAvailability.day,
+                    startTime: workerDetails.workerAvailability.startTime
+                }
+            })
+        }
+    }, [workerDetails])
+
+    useEffect(() => {
+        fetchWorkerDetail();
+        fetchServices();
+    }, []);
+
+    const handleScheduleCancel = () => {
+        setNewWorkerSchedule({
+            serviceCategory: workerDetails.serviceCategory,
+            workerAvailability: {
+                areaAssigned: workerDetails.workerAvailability.areaAssigned,
+                day: workerDetails.workerAvailability.day,
+                startTime: workerDetails.workerAvailability.startTime
+            }
+        });
+        setEditMode(false);
+    };
+
+    const handleScheduleSave = async () => {
+        try {
+            const token = localStorage.getItem("token");
+
+            const response = await axios.put(`http://localhost:5000/api/worker/editWorkerServiceInformation`,
+                newWorkerSchedule,
+                {headers: {
+                    'Authorization': `Bearer ${token}`,
+                    'Content-Type': 'application/json'
+                }}
+            )
+
+            console.log("Saved: ", response);
+            fetchWorkerDetail();
+            setEditMode(false);
+        } catch (e) {
+            console.log(e)
+        }
+    }
+
     return (  
         <div className="schedule-page">
             <section className="schedule-container">
@@ -10,20 +203,30 @@ const WorkerSchedule = () => {
                             Service Type
                         </label>
                         <select 
-                            className="input" 
-                            name="serviceName" 
-                            // value={value}  
-                            id="service-input" 
-                            onChange={(e) => {
-                                onChange(e.target.value);  
-                            }}
-                        >
-                            {/* SELECT OPTIONS */}
-                            <option value="0">Select Service</option>
-                            {/* {data.map((s, index) => (
-                                <option key={index} value={s.serviceName}>{s.serviceName}</option>
-                            ))} */}
-                        </select>
+                        className="input" 
+                        name="serviceName" 
+                        value={newWorkerSchedule.serviceCategory}  
+                        id="service-input" 
+                        disabled ={!editMode}
+                        onChange={(e) => {
+                            const newServiceCategory = e.target.value;
+
+                            // Update the schedule and reset area selection
+                            setNewWorkerSchedule({
+                                ...newWorkerSchedule,
+                                serviceCategory: newServiceCategory,
+                                workerAvailability: {
+                                    ...newWorkerSchedule.workerAvailability,
+                                    areaAssigned: "0", // Reset area selection
+                                },
+                            });
+                        }}
+                    >
+                        {/* SELECT OPTIONS */}
+                        {serviceList.map((s, index) => (
+                            <option key={index} value={s.serviceName}>{s.serviceName}</option>
+                        ))}
+                    </select>
                     </div>
 
                     {/* AREA TYPE */}
@@ -36,6 +239,7 @@ const WorkerSchedule = () => {
                             name="sizeOfArea" 
                             // value={value}  
                             id="area-size-input" 
+                            disabled ={!editMode}
                             onChange={(e) => {
                                 onChange(e.target.value);  
                             }}
@@ -52,29 +256,64 @@ const WorkerSchedule = () => {
             <section className="schedule-container">
                 <h2>Worker Schedule</h2>
                 <div className="schedule-inputs" id="schedule-inputs">
-                    <table>
-                        <thead>
-                            <tr>
-                                <th>Service Category</th>
-                                <th>SUN</th>
-                                <th>MON</th>
-                                <th>TUE</th>
-                                <th>WED</th>
-                                <th>THU</th>
-                                <th>FRI</th>
-                                <th>SAT</th>
+                    <table className="schedule-table">
+                        <thead className="schedule-thead">
+                            <tr className="schedule-tr">
+                                <th className="service-header">Service Category</th>
+                                <th><label htmlFor="SUN">SUN</label></th>
+                                <th><label htmlFor="MON">MON</label></th>
+                                <th><label htmlFor="TUE">TUE</label></th>
+                                <th><label htmlFor="WED">WED</label></th>
+                                <th><label htmlFor="THU">THU</label></th>
+                                <th><label htmlFor="FRI">FRI</label></th>
+                                <th><label htmlFor="SAT">SAT</label></th>
                             </tr>
                         </thead>
-                        <tbody>
-                            <tr>
-                                <td></td>
-                                <td><input type="checkbox" /></td>
-                                <td><input type="checkbox" /></td>
-                                <td><input type="checkbox" /></td>
-                                <td><input type="checkbox" /></td>
-                                <td><input type="checkbox" /></td>
-                                <td><input type="checkbox" /></td>
-                                <td><input type="checkbox" /></td>
+                        <tbody className="schedule-tbody">
+                            <tr className="schedule-tr">
+                                <td>{newWorkerSchedule.serviceCategory}</td>
+                                <td>
+                                    <input type="checkbox" name="worker-days" id="SUN" value="Sunday"
+                                    onChange={handleDayChanges}
+                                    disabled ={!editMode}
+                                    checked={newWorkerSchedule?.workerAvailability?.day.includes("Sunday")}/>
+                                </td>
+                                <td>
+                                    <input type="checkbox" name="worker-days" id="MON" value="Monday"
+                                    onChange={handleDayChanges}
+                                    disabled ={!editMode}
+                                    checked={newWorkerSchedule?.workerAvailability?.day.includes("Monday")}/>
+                                </td>
+                                <td>
+                                    <input type="checkbox" name="worker-days" id="TUE" value="Tuesday"
+                                    onChange={handleDayChanges}
+                                    disabled ={!editMode}
+                                    checked={newWorkerSchedule?.workerAvailability?.day.includes("Tuesday")}/>
+                                </td>
+                                <td>
+                                    <input type="checkbox" name="worker-days" id="WED" value="Wednesday"
+                                    onChange={handleDayChanges}
+                                    disabled ={!editMode}
+                                    checked={newWorkerSchedule?.workerAvailability?.day.includes("Wednesday")}/>
+                                </td>
+                                <td>
+                                    <input type="checkbox" name="worker-days" id="THU" value="Thursday"
+                                    onChange={handleDayChanges}
+                                    disabled ={!editMode}
+                                    checked={newWorkerSchedule?.workerAvailability?.day.includes("Thursday")}/>
+                                </td>
+                                <td>
+                                    <input type="checkbox" name="worker-days" id="FRI" value="Friday"
+                                    onChange={handleDayChanges}
+                                    disabled ={!editMode}
+                                    checked={newWorkerSchedule?.workerAvailability?.day.includes("Friday")}/>
+                                </td>
+                                <td>
+                                    <input type="checkbox" name="worker-days" id="SAT" value="Saturday"
+                                    onChange={handleDayChanges}
+                                    disabled ={!editMode}
+                                    checked={newWorkerSchedule?.workerAvailability?.day.includes("Saturday")}/>
+                                </td>
                             </tr>
                         </tbody>
                     </table>
@@ -83,9 +322,50 @@ const WorkerSchedule = () => {
             <section className="schedule-container">
                 <h2>Time Availability</h2>
                 <div className="schedule-inputs" id="timeavail-inputs">
-
+                {timeList.map((time, index) => (
+                <div className="time-button-container" key={index}>
+                    <input 
+                    type="checkbox" 
+                    name="worker-times"
+                    id={time} 
+                    className="worker-time-checkbox"
+                    value={time}
+                    onChange={handleTimeChanges}
+                    disabled ={!editMode}
+                    checked={newWorkerSchedule?.workerAvailability?.startTime.includes(time)}/>
+                    <label 
+                    htmlFor={time} 
+                    className="worker-time-button">
+                        {convertTime(time)}
+                    </label>
+                </div>
+                ))}
                 </div>
             </section>
+            <div className="schedule-actions">
+            {!editMode ?
+                <button
+                type="button"
+                className="schedule-btn green"
+                onClick={(e) => setEditMode(true)}>
+                    Edit Schedule
+                </button> :
+                <>
+                    <button
+                    type="button"
+                    className="schedule-btn red"
+                    onClick={handleScheduleCancel}>
+                        Cancel
+                    </button>
+                    <button
+                    type="button"
+                    className="schedule-btn green"
+                    onClick={handleScheduleSave}>
+                        Save Changes
+                    </button>
+                </>
+            }
+            </div>
         </div>
     );
 }
