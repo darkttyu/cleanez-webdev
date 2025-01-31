4b150ae09ea791c99811f9eb5f6650cbb62ef33f
darkttyu <ctrlfrz0710@gmail.com>
2025-01-31 12:17:37 +0800

    backend admin commit

--- backend/services/admin.service.js
+++ backend/services/admin.service.js
@@ -226,8 +226,9 @@ export const updateStatus = async(userId) => {
 };
 
 export const serviceDeleteWorker = async(userId) => {
-  const checkWorkerAppointment = await Worker.findOne({ userId })
-    if(checkWorkerAppointment.assignedAppointments != []){
+  const checkWorkerAppointment = await Worker.findOne({userId: new Object(userId) });
+  console.log(checkWorkerAppointment)
+    if(checkWorkerAppointment.assignedAppointments.length > 0){
       throw new Error("Cannot Delete Worker. Worker still has Assigned Appointments")
     }
   
