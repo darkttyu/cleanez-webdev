e2397aa3ec7e4e71cff2c44f37a974cd1d7fc0bb
darkttyu <ctrlfrz0710@gmail.com>
2025-01-31 13:40:46 +0800

    typo fixed hosting

--- backend/services/admin.service.js
+++ backend/services/admin.service.js
@@ -226,7 +226,7 @@ export const updateStatus = async(userId) => {
 };
 
 export const serviceDeleteWorker = async(userId) => {
-  const checkWorkerAppointment = await Worker.findOne({userId: userid});
+  const checkWorkerAppointment = await Worker.findOne({userId: userId});
     if(checkWorkerAppointment.assignedAppointments.length > 0){
       throw new Error("Cannot Delete Worker. Worker still has Assigned Appointments")
     }
--- frontend/src/pages/UserSigningPages/FindAccountPage1.js
+++ frontend/src/pages/UserSigningPages/FindAccountPage1.js
@@ -61,7 +61,7 @@ const FindAccountPanel = () => {
                             size="162" 
                             color="#06E36D"
                             />
-                            <h2>Password Successfully Requested.</h2>
+                            <h2>Password Reset Successfully Requested.</h2>
                             <p className="description">Please check your e-mail ( <strong><span>{email}</span></strong> ) to reset your password.
                             <br /><br />
                             You are now being redirected to the Home Page in 5 seconds...</p>
