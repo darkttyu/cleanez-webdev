ca2235c166a1c11ae165de415248530e8e13a3c6
darkttyu <ctrlfrz0710@gmail.com>
2025-02-07 01:16:33 +0800

    merge to host

--- backend/controllers/appointment.controller.js
+++ backend/controllers/appointment.controller.js
@@ -365,9 +365,11 @@ export const setAppointment = async (req, res) => {
             const workerTime = formatTime(scheduleDetails.startTime);
             const earnings = serviceCost / serviceDetails.numberOfWorkers
             
+            const customerInfo = await User.findById(userId);
+
             // Sends the appointment confirmation email to the worker
             sendWorkerAppointmentConfirmation(
-              user.firstName, user.email, customerFirstName, 
+              user.firstName, customerInfo.email, customerFirstName, 
               customerLastName, address.block, address.municipal,
               address.province, address.barangay, serviceDetails.serviceCategory,
               serviceDetails.sizeOfArea, scheduleDetails.date, 
